7b351ac486f809cbcd6352dd801dad703b17ac3e
JeferssonCL <jefersson.coronel.13622689@usalesiana.edu.bo>
2024-10-13 02:19:32 -0400

    feat: adding concrete entities for each entity in our data base

---
+++ Backend/Backend.Domain/Entities/Concretes/PaymentTransaction.cs
@@ -0,0 +1,13 @@
+using Backend.Domain.Entities.Bases;
+using Backend.Domain.Entities.Enums;
+
+namespace Backend.Domain.Entities.Concretes;
+
+public class PaymentTransaction : BaseEntity
+{
+    public Guid OrderId { get; set; }
+    public PaymentMethod PaymentMethod { get; set; }
+    public Status TransactionStatus { get; set; }
+    public double Amount { get; set; }
+    public Order Order { get; set; }
+}
