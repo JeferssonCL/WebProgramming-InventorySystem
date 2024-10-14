93fb3839df5d756d7b652ef1c4d2727e0186ae11
gandel <denis-gandel>
2024-10-14 09:30:49 -0400

    feat: connect with api

--- Frontend/src/components/ProductCard.jsx
+++ Frontend/src/components/ProductCard.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import '../styles/components/productCard.css'
 import { FaMoneyBill, FaCartPlus, FaTag } from "react-icons/fa";
 
-export function ProductCard({ id, name, price, brand, image }) {
+export function ProductCard({ id, name, price, brand, image, onAddToCart }) {
     return (
         <div className="product-card">
             <img className='product-card-image' src={image} alt={`Product image: ${name}`} />
@@ -11,7 +11,7 @@ export function ProductCard({ id, name, price, brand, image }) {
             <p className='product-card-brand'>
                 <FaTag /> {brand}
             </p>
-            <button className="product-card-add-shop-cart">
+            <button className="product-card-add-shop-cart" onClick={onAddToCart}>
                 <FaCartPlus /> Add to cart
             </button>
         </div>
@@ -23,5 +23,6 @@ ProductCard.propTypes = {
     name: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired,
     brand: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired
+    image: PropTypes.string.isRequired,
+    onAddToCart: PropTypes.func.isRequired
 };
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,14 +1,64 @@
-import { useState } from "react"
-import { ProductCard } from "../components/ProductCard"
+import { useState, useEffect } from "react";
+import { ProductCard } from "../components/ProductCard";
+import axios from 'axios';
+import '../styles/pages/home.css';
 
 export function Home() {
+    const [productList, setProductList] = useState([]);
+    const [cart, setCart] = useState([]);
 
-    const [productList, setProductList] = useState([])
+    useEffect(() => {
+        const apiUrl = "http://localhost:5163/api/product?page=1&limit=20";
+
+        const fetchProducts = async () => {
+            try {
+                const response = await axios.get(apiUrl);
+                setProductList(response.data.data);
+            } catch (error) {
+                console.error("Error fetching products:", error);
+            }
+        };
+
+        fetchProducts();
+    }, []);
+
+    const handleAddToCart = (product) => {
+        const productToStore = {
+            id: product.id,
+            name: product.name,
+            price: product.price,
+            image: product.image,
+        };
+
+        const productExists = cart.find(item => item.id === productToStore.id);
+        if (!productExists) {
+            const updatedCart = [...cart, productToStore];
+            setCart(updatedCart);
+            localStorage.setItem('cart', JSON.stringify(updatedCart));
+        } else {
+            console.log("The product is already in the cart");
+        }
+    };
 
     return (
-
         <div className="products-section">
-            <ProductCard id="123456" name="Product example" price={10} brand="Folladores" image="https://cdn.shopify.com/s/files/1/0070/7032/files/product-label-design.jpg?v=1680902906" />
+            {
+                productList.length === 0 ? (
+                    <p>No products available at the moment :c</p>
+                ) : (
+                    productList.map(product => (
+                        <ProductCard
+                            key={product.id}
+                            id={product.id}
+                            name={product.name}
+                            brand={product.brand}
+                            price={product.price}
+                            image={product.images[0].url}
+                            onAddToCart={() => handleAddToCart(product)}
+                        />
+                    ))
+                )
+            }
         </div>
-    )
+    );
 }
