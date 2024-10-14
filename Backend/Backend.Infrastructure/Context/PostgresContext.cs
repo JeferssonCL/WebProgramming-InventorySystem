3a6645d33549a14d45f7c10158b168c7e3a24b61
JeferssonCL <jefersson.coronel.13622689@usalesiana.edu.bo>
2024-10-13 21:58:38 -0400

    refactor: refactor context to manage type confirgurations for each entity

--- Backend/Backend.Infrastructure/Context/PostgresContext.cs
+++ Backend/Backend.Infrastructure/Context/PostgresContext.cs
@@ -1,20 +1,19 @@
-using Backend.Domain.Entities.Concretes;
+using Backend.Domain.Entities.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Infrastructure.Context;
 
 public class PostgresContext(DbContextOptions<PostgresContext> options) : DbContext(options)
 {
-    public DbSet<Category> Categories { get; set; }
-    public DbSet<Image> Images { get; set; }
-    public DbSet<Order> Orders { get; set; }
-    public DbSet<OrderItem> OrderItems { get; set; }
-    public DbSet<PaymentTransaction> PaymentTransactions { get; set; }
-    public DbSet<Product> Products { get; set; }
-    public DbSet<ProductAttribute> ProductAttributes { get; set; }
-    public DbSet<Shipment> Shipments { get; set; }
-    public DbSet<Store> Stores { get; set; }
-    public DbSet<User> Users { get; set; }
-    public DbSet<UserAddress> UserAddresses { get; set; }
-    public DbSet<Variant> Variants { get; set; }
+    public DbSet<TEntity> Set<TEntity>() where TEntity : class, IEntity
+    {
+        return base.Set<TEntity>();
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(typeof(PostgresContext).Assembly);
+        
+        base.OnModelCreating(builder);
+    }
 }
