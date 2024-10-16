12abef6926f31d44d693cd146a9693c07f05bb97
JeferssonCL <jefersson.coronel.13622689@usalesiana.edu.bo>
2024-10-16 03:39:22 -0400

    feat: adding endpoint

---
+++ Backend/Backend.Api/Controllers/ShoppingCartController.cs
@@ -0,0 +1,38 @@
+using AutoMapper;
+using Backend.Application.Dtos;
+using Backend.Application.Dtos.checkoutSession;
+using Backend.Application.Handlers.Checkout_Session.Request.Commands;
+using Backend.Domain.Entities.Concretes;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ShoppingCartController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    private readonly IMapper _mapper;
+
+    public ShoppingCartController(IMediator mediator, IMapper mapper)
+    {
+        _mediator = mediator;
+        _mapper = mapper;
+    }
+
+    [HttpPost]
+    public ActionResult<Dictionary<string, bool>> InitCheckoutSession(List<ShoppingCartItemDto> shoppingCartItemDtos)
+    {
+        foreach (var shoppingCartItemDto in shoppingCartItemDtos)
+            Console.WriteLine(shoppingCartItemDto);
+
+        var result = _mediator.Send(new CreateCheckoutSessionCommand(shoppingCartItemDtos));
+
+        return Ok(new Dictionary<string, bool>
+        {
+            { "result", result.Result }
+        });
+    }
+
+}
