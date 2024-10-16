420ebd4f0cdd684c92a3be1737ac89a3f4155608
gandel <denis-gandel>
2024-10-16 07:35:13 -0400

    fix: fix button increase and decrease

--- Frontend/src/components/StockQuantityInput.jsx
+++ Frontend/src/components/StockQuantityInput.jsx
@@ -5,11 +5,11 @@ import '../styles/components/stockQuantityInput.css'
 export function StockQuantityInput({ id, quantity, increse, decrese }) {
   return (
     <div className="stock-quantity-input">
-      <button className="quantity-input-decrese-button" onClick={() => decrese()}>
+      <button className="quantity-input-decrese-button" onClick={() => decrese(id)}>
         <FaMinus />
       </button>
       <input type="number" name="quantity-input" id="quantity-input" className="quantity-input" value={quantity} />
-      <button className="quantity-input-decrese-button" onClick={() => increse()}>
+      <button className="quantity-input-decrese-button" onClick={() => increse(id)}>
         <FaPlus />
       </button>
     </div>
