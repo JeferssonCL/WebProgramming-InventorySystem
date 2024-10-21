417f5ed16cc40c32a46f9f699c1f36d55a0b1c99
JeferssonCL <jefersson.coronel.13622689@usalesiana.edu.bo>
2024-10-21 02:06:29 -0400

    feat: finish implementation post in frontend to manage creation of order

--- Frontend/src/pages/PaymentStatusSuccess.jsx
+++ Frontend/src/pages/PaymentStatusSuccess.jsx
@@ -1,8 +1,59 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import '../styles/components/success.css';
+import { useLocation, useNavigate } from 'react-router-dom';
 import successImage from '../assets/succesImage.png';
 
 const PaymentStatusSuccess = () => {
+    const location = useLocation();
+    const query = new URLSearchParams(location.search);
+    const sessionId = query.get('session_id');
+
+    const hasRunRef = useRef(false);
+    const navigate = useNavigate();
+
+    const submitOrder = async () => {
+      const customer = {
+        id: "c4055860-c902-4787-ba54-0b34e18a1040",
+        email: "customer@example.com",
+        address : "default",
+        city: "default",
+        country: "default"
+      };
+
+      const requestBody = {
+        StripeSessionId: sessionId,
+        Customer: customer,
+      };
+      const response = await fetch('http://localhost:5163/api/Order', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(requestBody)
+      });
+
+      if (!response.ok) {
+          const errorMessage = await response.text();
+          console.error("Error from API:", errorMessage);
+          return;
+      }
+
+      localStorage.setItem('orderSubmitted', 'true');
+    };
+
+    useEffect(() => {
+      const orderSubmitted = localStorage.getItem('orderSubmitted');
+      if (sessionId && !hasRunRef.current && !orderSubmitted) {
+        hasRunRef.current = true;
+        submitOrder();
+      }
+    }, [sessionId]);
+
+    const handleContinueShopping = () => {
+      localStorage.removeItem('orderSubmitted');
+      localStorage.removeItem('products');
+      localStorage.removeItem('cart');
+      navigate('/');
+      window.location.reload();
+    };
 
     return (
         <div className="success-container">
@@ -13,7 +64,7 @@ const PaymentStatusSuccess = () => {
 
                 <div className="success-actions">
                     <h3 className="action-title">What to do next?</h3>
-                    <button className="continue-shopping-button">Continue shopping</button>
+                    <button className="continue-shopping-button" onClick={handleContinueShopping}>Continue shopping</button>
                     <button className="view-order-button">View my order</button>
                 </div>
             </div>
